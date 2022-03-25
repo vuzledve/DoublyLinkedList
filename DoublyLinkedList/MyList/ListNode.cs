ffdf27de64802d1dd86e4364771b10c207853dcc
Дмитрий Б <vuzledve@mail.ru>
2022-03-25 23:41:55 +0300

    Добавлены методы addNode и GetRandomNode

--- DoublyLinkedList/MyList/ListNode.cs
+++ DoublyLinkedList/MyList/ListNode.cs
@@ -8,9 +8,9 @@ namespace DoublyLinkedList.MyList
 {
     public class ListNode
     {
-        public ListNode Previous;
-        public ListNode Next;
-        public ListNode Random; // произвольный элемент внутри списка
+        public ListNode? Previous;
+        public ListNode? Next;
+        public ListNode? Random; // произвольный элемент внутри списка
         public string Data;
     }
 }
--- DoublyLinkedList/MyList/ListRandom.cs
+++ DoublyLinkedList/MyList/ListRandom.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace DoublyLinkedList.MyList
 {
+    
     class ListRandom
     {
         public ListNode? Head;
@@ -17,6 +18,37 @@ namespace DoublyLinkedList.MyList
             Head = null;
             Tail = null;
             Count = 0;
+        }
+
+        public void addNode(ListNode newNode) //вставка в конец списка
+        {
+            newNode.Next = null; //тк список не кольцевой и добавляем всегда в конец - ссылка на след. эл.=null
+            
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;               
+                newNode.Previous = null;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                newNode.Previous = Tail;
+                Tail = newNode;
+            }
+            newNode.Random = GetRandomNode();
+            Count++;
+        }
+
+        private ListNode GetRandomNode()
+        {
+            ListNode node = Head;
+            int rnd = new Random().Next(Count);
+            for (int i = 0; i < rnd; i++)
+            {
+                node= node.Next;
+            }
+            return node;
         }
 
         public void Serialize(Stream s)
